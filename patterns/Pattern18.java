da6a3bb2ae7f8427a335ccfc0cb25772d70ce0c4
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-12 18:36:32 +0530

    added Pattern18

---
+++ patterns/Pattern18.java
@@ -0,0 +1,28 @@
+// A
+// ABA
+// ABCBA
+// ABCDCBA
+// ABCDEDCBA
+// ABCDEFEDCBA
+package patterns;
+import java.util.*;
+
+public class Pattern18 {
+    public void pattern18(int n) {
+        char lastChar = (char) ('A' + n - 1);  
+        for (int i = 0; i < n; i++) {  
+            for (char ch = (char) (lastChar - i); ch <= lastChar; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        Pattern18 p18 = new Pattern18();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n = sc.nextInt();
+        p18.pattern18(n);
+        sc.close();
+    }
+}
