9922465416f0a2991e4c1d2cc04d71cbc172f703
ShrutikaChavan22 <shrutikachavan2211@gmail.com>
2026-02-12 19:39:38 +0530

    added Pattern19

---
+++ patterns/Pattern19.java
@@ -0,0 +1,45 @@
+package patterns;
+import java.util.*;
+
+public class Pattern19{
+
+    public void pattern19(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (i - 1); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        Pattern19 p19 = new Pattern19();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any number:");
+        int n = sc.nextInt();
+
+        p19.pattern19(n);
+        sc.close();
+    }
+}
